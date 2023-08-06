36d9f1bb6d6d27881d03408bfc4967ce7143af28
Valentina Rogojina <rogojinavaliusha@gmail.com>
2023-08-06 16:28:34 +0300

    Exemplu4

---
+++ Exemplu4/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+
+
+// Ввод числа N с клавиатуры
+            Console.Write(Console"Введите число : "); 
+
+            int N = int.Parse(Console.ReadLine());
+
+            // Перебор чисел от 1 до N.
+
+            for (int i = 1; i <= N; i++);
+
+            // Проверка на чётность
+                
+               
+            if (i % 2 == 0)
+                {
+                    Console.WriteLine(result);
+                }
+
+                {
+                    Console.WriteLine(result);
+                }
+
+                {
+                    Console.WriteLine(result);
+                }
+
+
+
